16e7cd966a3dd7e5753a551164efc61d59e523f7
Pratik <pratikchandlekar280@gmail.com>
2021-08-22 20:49:11 +0530

    gedit nord xml

--- symlinkscript.sh
+++ symlinkscript.sh
@@ -3,5 +3,10 @@
 # .bashrc
 # first delete default .bashrc
 rm -rf ~/.bashrc
-ln -s ~/dotfile/.bashrc ~/.bashrc
+ln -s ~/dotfiles/.bashrc ~/.bashrc
 
+# nord theme gedi
+rm -rf ~/.config/gedit
+mkdir ~/.config/gedit
+# touch ~/.config/gedit/nord.xml
+ln -s ~/dotfiles/config/nord.xml ~/.config/gedit/nord.xml
